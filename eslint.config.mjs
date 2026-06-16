49b92186600017aa93736391a7db7f77802839a6
Khoioioiii <khoi369@unc.edu>
2026-06-16 04:52:39 -0400

    Document Vercel deployment visibility setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import coreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
+  {
+    ignores: [".next/**", ".vercel/**", "node_modules/**", "out/**", "build/**"]
+  },
   ...coreWebVitals,
   ...nextTypescript
 ];
